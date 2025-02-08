da6869e08ceba3ab9cf9f4a159d64bb6e0596778
hyeon48615 <hyeon48615@gmail.com>
2025-02-08 23:50:14 +0900

    Update 자동차 대여 기록에서 장기／단기 대여 구분하기.sql

--- MySQL/프로그래머스/1/151138. 자동차 대여 기록에서 장기／단기 대여 구분하기/자동차 대여 기록에서 장기／단기 대여 구분하기.sql
+++ MySQL/프로그래머스/1/151138. 자동차 대여 기록에서 장기／단기 대여 구분하기/자동차 대여 기록에서 장기／단기 대여 구분하기.sql
@@ -1,6 +1,7 @@
 -- 코드를 입력하세요
 SELECT history_id, car_id, date_format(start_date, '%Y-%m-%d'), date_format(end_date, '%Y-%m-%d')
      , (case when datediff(end_date, start_date) >= 29 then '장기 대여' else '단기 대여' end) as RENT_TYPE
+# 주의. 30일 이상 => (case when ~) >= 29
 from car_rental_company_rental_history
 where start_date like '2022-09-%'
 order by history_id desc
