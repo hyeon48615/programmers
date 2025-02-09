1a0220026ee1620db0c6a1bbc958dcd50c6f5e5b
hyeon48615 <hyeon48615@gmail.com>
2025-02-09 21:12:37 +0900

    [level 1] Title: 상위 n개 레코드, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- MySQL/프로그래머스/1/59405. 상위 n개 레코드/상위 n개 레코드.sql
+++ MySQL/프로그래머스/1/59405. 상위 n개 레코드/상위 n개 레코드.sql
@@ -1,5 +1,10 @@
 -- 코드를 입력하세요
+# select name
+# from animal_ins
+# where datetime = (select min(datetime)
+#                   from animal_ins)
+
 select name
 from animal_ins
-where datetime = (select min(datetime)
-                  from animal_ins)
+order by datetime
+limit 1
