729adb8d20d0a7b24472f10152438e4ea21531a6
hyeon48615 <hyeon48615@gmail.com>
2025-02-09 21:07:39 +0900

    Update 경기도에 위치한 식품창고 목록 출력하기.sql

--- MySQL/프로그래머스/1/131114. 경기도에 위치한 식품창고 목록 출력하기/경기도에 위치한 식품창고 목록 출력하기.sql
+++ MySQL/프로그래머스/1/131114. 경기도에 위치한 식품창고 목록 출력하기/경기도에 위치한 식품창고 목록 출력하기.sql
@@ -1,5 +1,6 @@
 -- 코드를 입력하세요
 select warehouse_id, warehouse_name, address, ifnull(freezer_yn, 'N')
 from food_warehouse
-where warehouse_name like '%경기%'
+where address like '%경기%'
+# where warehouse_name like '%경기%'
 order by warehouse_id
